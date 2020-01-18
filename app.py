36f603e46807a9b9ee2b8a12b4f3ca3bb5e9e1fd
Tom Shen <tom.shen@tgc-taiwan.com.tw>
2020-01-19 02:38:02 +0800

    是也V1

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message+"是也")
+    message = TextSendMessage(text=event.message.text + "是也")
+    line_bot_api.reply_message(event.reply_token, message)
 
 import os
 if __name__ == "__main__":
