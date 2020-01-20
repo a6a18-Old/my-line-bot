47c34f4aa13d6540f2c73351bd180f6090305c96
Tom Shen <tom.shen@tgc-taiwan.com.tw>
2020-01-21 00:33:18 +0800

    添加新聞

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+import requests
+import re
+import random
+import configparser
+from bs4 import BeautifulSoup
 from flask import Flask, request, abort
+from imgurpython import ImgurClient
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -30,11 +36,32 @@ def callback():
         abort(400)
     return 'OK'
 
+def apple_news():
+    target_url = 'https://tw.appledaily.com/new/realtime'
+    print('Start parsing appleNews....')
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ""
+    for index, data in enumerate(soup.select('.rtddt a'), 0):
+        if index == 5:
+            return content
+        link = data['href']
+        content += '{}\n\n'.format(link)
+    return content
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text + "是也")
-    line_bot_api.reply_message(event.reply_token, message)
+    print("event.reply_token:", event.reply_token)
+    print("event.message.text:", event.message.text)
+
+    if event.message.text == "蘋果即時新聞":
+        content = apple_news()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
 
 import os
 if __name__ == "__main__":
